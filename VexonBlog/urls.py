2255ba0a887947f5f6fefc87ed5836ca0549cc80
Behzad <behzadmohamady4545@gmail.com>
2025-04-07 22:00:13 +0330

    add login

--- VexonBlog/urls.py
+++ VexonBlog/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from . import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name='base.html'),name='home'),
     path('accounts/',include('accounts.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,23 @@
 from django import forms 
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate
+from django.forms import ValidationError
 
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username','email','password']
+
+        
+
+class LoginForm(forms.Form):
+    
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    
+
+
+
+       
