d571b27380f2066b86f9d3a668860f3e47b45fa0
Ali <jeneral1385@gmail.com>
2025-04-26 11:34:20 +0330

    added contact us

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SiteSettings, Newsletter
+from .models import SiteSettings, Newsletter, ContactUs
 
 
 admin.site.register(SiteSettings)
 admin.site.register(Newsletter)
+admin.site.register(ContactUs)
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'core'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('newsletter/', views.newsletter, name='newsletter'),
+    path('contactus', views.contactus, name='contactus'),
 ]
